24faa8a371560ae5b0df3c851bacd1e47c5a1f34
Guslington <guslington@gmail.com>
2019-05-27 15:49:21 +1000

    add listener

--- network-loadbalancer.cfndsl.rb
+++ network-loadbalancer.cfndsl.rb
@@ -26,35 +26,64 @@ CloudFormation do
     end
   end
 
-  targetgroups.each do |tg_name, tg|
+  targetgroups.each do |tg_name, params|
 
     ElasticLoadBalancingV2_TargetGroup("#{tg_name}TargetGroup") {
       VpcId Ref(:VpcId)
       Protocol 'TCP'
-      Port tg['port']
+      Port params['port']
 
-      TargetType tg['type'] if tg.has_key?('type')
+      TargetType params['type'] if params.has_key?('type')
 
-      if tg.has_key?('type') and tg['type'] == 'ip' and tg.has_key? 'target_ips'
-        Targets (tg['target_ips'].map {|ip|  { 'Id' => ip['ip'], 'Port' => ip['port'] }})
+      if params.has_key?('type') and params['type'] == 'ip' and params.has_key? 'target_ips'
+        Targets (params['target_ips'].map {|ip|  { 'Id' => ip['ip'], 'Port' => ip['port'] }})
       end
 
-      if tg.has_key?('attributes')
-        TargetGroupAttributes tg['attributes'].map { |key,value| { Key: key, Value: value } }
+      if params.has_key?('attributes')
+        TargetGroupAttributes params['attributes'].map { |key,value| { Key: key, Value: value } }
       end
 
-      if tg.has_key?('healthcheck')
-        HealthCheckPort tg['healthcheck']['port'] if tg['healthcheck'].has_key?('port')
+      if params.has_key?('healthcheck')
+        HealthCheckPort params['healthcheck']['port'] if params['healthcheck'].has_key?('port')
         HealthCheckProtocol 'TCP'
-        HealthCheckIntervalSeconds tg['healthcheck']['interval'] if tg['healthcheck'].has_key?('interval')
-        HealthCheckTimeoutSeconds tg['healthcheck']['timeout'] if tg['healthcheck'].has_key?('timeout')
-        HealthyThresholdCount tg['healthcheck']['heathy_count'] if tg['healthcheck'].has_key?('heathy_count')
-        UnhealthyThresholdCount tg['healthcheck']['unheathy_count'] if tg['healthcheck'].has_key?('unheathy_count')
+        HealthCheckIntervalSeconds params['healthcheck']['interval'] if params['healthcheck'].has_key?('interval')
+        HealthCheckTimeoutSeconds params['healthcheck']['timeout'] if params['healthcheck'].has_key?('timeout')
+        HealthyThresholdCount params['healthcheck']['heathy_count'] if params['healthcheck'].has_key?('heathy_count')
+        UnhealthyThresholdCount params['healthcheck']['unheathy_count'] if params['healthcheck'].has_key?('unheathy_count')
       end
 
       Tags default_tags
     }
-  end
+  end if defined? targetgroups
+
+  listeners.each do |listener_name, params|
+
+    ElasticLoadBalancingV2_Listener("#{listener_name}Listener") {
+      Protocol params['protocol'].upcase
+      Port params['port']
+      LoadBalancerArn Ref(:NetworkLoadBalancer)
+
+      if params['protocol'].upcase == 'TLS'
+        certificate = params['certificates'].shift()
+        puts  certificate
+        Certificates [{ CertificateArn: FnSub("${#{certificate}}") }]
+        SslPolicy params['ssl_policy'] if params.has_key?('ssl_policy')
+      end
+
+      DefaultActions ([
+          TargetGroupArn: Ref("#{params['targetgroup']}TargetGroup"),
+          Type: "forward"
+      ])
+    }
+
+    if (params.has_key?('certificates')) && (params['protocol'].upcase == 'TLS') && (params['certificates'].any?)
+      ElasticLoadBalancingV2_ListenerCertificate("#{listener_name}ListenerCertificate") {
+        Certificates params['certificates'].map { |certificate| { CertificateArn: FnSub("${#{certificate}}") }  }
+        ListenerArn Ref("#{listener_name}Listener")
+      }
+    end
+
+  end if defined? listeners
 
   records.each do |record|
     name = ('apex' || '')? dns_format : "#{record}.#{dns_format}."
